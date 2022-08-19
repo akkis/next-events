9c34ec4c55c6776fb4dfe019c98af039a0d875fc
Akis Laoutaris <alaoutaris@nelios.com>
2022-08-19 11:44:41 +0300

    course 80

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,15 @@
+import EventList from "../../components/events/EventList";
+import EventsSearch from "../../components/events/events-search";
+import {getAllEvents} from "../../dummy-data";
+
 export default function AllEventsPage() {
+  const events = getAllEvents();
+
   return (
     <div>
       <h2>All events...</h2>
+      <EventsSearch />
+      <EventList items={events} />
     </div>
   );
 }
