aeb1e32656f7eea07018bc89024857a0f56261a9
Akis Laoutaris <alaoutaris@nelios.com>
2022-08-19 12:22:10 +0300

    course 82

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,17 +1,39 @@
 import { useRouter } from "next/router";
+import { Fragment } from "react";
+import EventList from "../../components/events/EventList";
+import { getFilteredEvents } from '../../dummy-data';
 
 export default function FilteredEventsPage() {
   const router = useRouter();
 
-  console.log(router.query);
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (isNaN(numYear) || isNaN(numMonth) || numYear > 2030 || numYear < 2021 || numMonth < 1 || numMonth > 12) {
+    return <p>Invalid filter. Please abjust your values!</p>
+  }
+
+  const filteredEvents = getFilteredEvents({year: numYear, month: numMonth})
+
+  if (! filteredEvents || filteredEvents.length === 0) {
+    return <p>No Events found for the chosen filter!</p>
+  }
 
   return (
-    <div>
-      <h1>Fitlered:</h1>
-      <p>custom filter:</p>
-      {router.query.slug && router.query.slug.map((f) => (
-        <li key={f}>{f}</li>
-      ))}
-    </div>
+    <Fragment>
+      <h1 className="center">Showing events with filters:</h1>
+      <p className="center">Year: {filteredYear}</p>
+      <p className="center">Month: {filteredMonth}</p>
+      <EventList items={filteredEvents} />
+    </Fragment>
   );
 }
